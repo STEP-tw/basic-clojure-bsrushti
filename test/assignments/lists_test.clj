305e19484d3320f5bf8f5cb129b40483a10f29ed
bsrushti <bsrushti@thoughtworks.com>
2020-02-03 01:07:21 +0530

    Implement filter

--- test/assignments/lists_test.clj
+++ test/assignments/lists_test.clj
@@ -2,10 +2,16 @@
   (:require [clojure.test :refer :all]
             [assignments.lists :refer :all]))
 
-(deftest lists
-  (testing "map"
-    (testing "identity with single coll"
-      (is (= [1 2 3] (map' identity [1 2 3]))))
-    (testing "inc with single coll"
-      (is (= [2 3 4] (map' inc [1 2 3])))))
-  )
+(deftest map'-test
+  (testing "identity with single coll"
+    (is (= [1 2 3] (map' identity [1 2 3]))))
+  (testing "inc with single coll"
+    (is (= [2 3 4] (map' inc [1 2 3])))))
+
+(deftest filter'-test
+  (testing "filter"
+    (testing "even?"
+      (is (= [2 4] (filter' even? [1 2 3 4])))))
+    (testing "odd?"
+      (is (= [1 3] (filter' odd? [1 2 3 4])))))
+
